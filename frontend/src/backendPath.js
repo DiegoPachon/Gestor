b42606a62b2da81102c5d391ee220676553ee5d5
DiegoPachon <79177336+DiegoPachon@users.noreply.github.com>
2023-05-15 19:08:25 -0500

    url backend

--- frontend/src/backendPath.js
+++ frontend/src/backendPath.js
@@ -1,4 +1,4 @@
-const URLBackend = "https://node-express23.herokuapp.com/";
+const URLBackend = "https://node-express23.herokuapp.com";
 export const cloudinary_name = "djlpt2ubq";
 //export const cloudinary_name = "dynkblhi3";
 
